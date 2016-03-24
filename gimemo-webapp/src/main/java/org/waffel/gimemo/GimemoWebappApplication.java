44d52ee7f246d0452ebb7c358f537e4fcddf514b
Thomas Wabner <thomas.wabner@siemens.com>
2016-03-24 13:49:06 +0100

    initial setup with spring boot and neurop dependencies

---
+++ gimemo-webapp/src/main/java/org/waffel/gimemo/GimemoWebappApplication.java
@@ -0,0 +1,12 @@
+package org.waffel.gimemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimemoWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimemoWebappApplication.class, args);
+	}
+}
