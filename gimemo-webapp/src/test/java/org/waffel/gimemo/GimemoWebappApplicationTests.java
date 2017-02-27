9064ffbe230bd4d1385cba7f3d2e3568ccdc3a35
Thomas Wabner <thomas.wabner@siemens.com>
2017-02-27 15:54:36 +0100

    using latest spring boot version 1.5.x and align spring boot application at all

--- gimemo-webapp/src/test/java/org/waffel/gimemo/GimemoWebappApplicationTests.java
+++ gimemo-webapp/src/test/java/org/waffel/gimemo/GimemoWebappApplicationTests.java
@@ -2,13 +2,10 @@ package org.waffel.gimemo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = GimemoWebappApplication.class)
-@WebAppConfiguration
+@RunWith(SpringRunner.class) @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GimemoWebappApplicationTests {
 
 	@Test
